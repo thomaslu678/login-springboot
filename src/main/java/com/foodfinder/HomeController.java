99ccbebc86ca3054959740f46f6463080963e865
thomaslu678 <thomaslu678@gmail.com>
2024-08-07 20:20:42 -0600

    @ResponseBody: string instead of template

--- src/main/java/com/foodfinder/HomeController.java
+++ src/main/java/com/foodfinder/HomeController.java
@@ -2,13 +2,17 @@ package com.foodfinder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index";
+    @ResponseBody // for returning string instead of template with that name
+    public String home(Principal principal) {
+        return "Hello".concat(principal.getName());
     }
 
     @GetMapping("/login")
