90d1af14da62176dfae2de6060464f00d9b32310
thomaslu678 <thomaslu678@gmail.com>
2024-08-08 21:39:21 -0600

    populate controllers

--- src/main/java/com/foodfinder/controller/HomeController.java
+++ src/main/java/com/foodfinder/controller/HomeController.java
@@ -1,13 +1,21 @@
-package com.foodfinder;
+package com.foodfinder.controller;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@Controller
+@RestController
+@CrossOrigin
 public class HomeController {
+
+    @PostConstruct
+    void init() {
+    }
 
     @GetMapping("/")
     @ResponseBody // for returning string instead of template with that name
@@ -16,6 +24,7 @@ public class HomeController {
     }
 
     @GetMapping("/login")
+//    @ResponseBody
     public String login() {
         return "login";
     }
