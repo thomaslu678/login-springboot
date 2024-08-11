4af17d159ac29c4a8629551ea04d9b79480b3487
thomaslu678 <thomaslu678@gmail.com>
2024-08-11 17:53:46 -0600

    login redirects to index.html

--- src/main/java/com/foodfinder/controller/HomeController.java
+++ src/main/java/com/foodfinder/controller/HomeController.java
@@ -19,9 +19,8 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    @ResponseBody // for returning string instead of template with that name
     public String home(Principal principal) {
-        return "Hello".concat(principal.getName());
+        return "index";
     }
 
     @PreAuthorize("hasRole('ADMIN')")
